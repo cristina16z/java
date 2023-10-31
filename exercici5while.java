1b2c209ed474ddc5e00d0d60756c97c8086fb9e4
cristina <2023_cristina.val@iticbcn.cat>
2023-10-31 12:00:28 +0100

    31/10/2023

--- exercici5while.java
+++ exercici5while.java
@@ -21,7 +21,7 @@ public class exercici5while {
     /*Input ejemplo: ssd 50 25.8 90 72 88 -1.0  (el -1.0 para indiciar que ya no habrá más precios) */
 
 
-/*EXERICICI: Donat botigues, preus i descomptes de cadascuna, retorni el nom de la botiga amb el millor Preu
+/*EXERICICI 2: Donat botigues, preus i descomptes de cadascuna, retorni el nom de la botiga amb el millor Preu
  *          input: botiga1 78.8 (preu) 20 (descompte)
  *                  botiga2 100 50
  *                  botiga 3 66 2
@@ -50,7 +50,74 @@ public class exercici5while {
         }
 
         System.out.println("La millor botiga és  " + millorBotiga + " amb un preu de " + millorPreu + "€");
-      
+
+/*EXERICICI 3:Dado una lista infinita de productos y precios de cada producto determinado su final con string Final y -1.0
+                La salida tiene que devolver la lista de productos diciendo cuál es el precio más bajo /económico.
+ 
+                input: ssd 10.3 11 32 21 15.3 21 -1.0           salida: ssd 10.3
+ *                  hdd 30 25 14.5 13.6 -1.0                        hdd 13.6
+ *                  portátil 529.99 630 700 677.4 -1.0              portátil 529.99
+ *                  final                                                                            */
+
+        final double MARCA_FINAL = -1.0;
+
+        String producte = entrada.next();
+        double millorPreu = 0.0;
+        double preu = 0.0;
+
+        while (!producte.equalsIgnoreCase("final")){
+            preu = entrada.nextDouble();
+            millorPreu = preu;
+            
+            while (preu != MARCA_FINAL) {
+                if  (preu < millorPreu){
+                        millorPreu = preu;
+                }
+                preu = entrada.nextDouble();
+            }
+            
+            System.out.println( producte + " el seu millor preu és " + millorPreu + "€");
+        producte = entrada.next();
     
+    }
+/*EXERICICI 5: Dado un número determinará la cantidad de tiendas a tener en cuenta 
+                3 (3 tiendas a tener en cuenta)
+                [nombre tienda] [precio] [descuento]
+                Y la salida es: de las tiendas a tener en cuenta, 
+                que salga el nombre de la más económica con el descuento aplicado. */
+                
+        int num = entrada.nextInt();
+        int contador = 0;
+        
+        String botiga = entrada.next();
+        double preu = entrada.nextDouble(); 
+        double descompte = entrada.nextDouble();
+                
+        String millorBotiga = botiga;
+        preu = preu*(100-descompte) / 100;
+        double millorPreu = preu;
+        double millorDescompte= descompte;
+                
+        contador++;
+        
+        while (contador < num) {    
+            botiga = entrada.next();
+            preu = entrada.nextDouble();              
+            descompte = entrada.nextDouble();  
+            preu = preu*(100-descompte) / 100;
+                    
+            if (preu < millorPreu) {                    
+                millorPreu = preu;
+                millorBotiga = botiga;
+                millorDescompte = descompte;
+            }
+                    
+            contador++;
+        }
+        
+        System.out.println("La millor botiga és " + millorBotiga + " amb un descompte de " + millorDescompte + "% amb un preu final de " + millorPreu + "€");
+         
+
+
     }
 }
