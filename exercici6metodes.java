f0afbc196fe53544a4d35c038786376266f1445f
cristina <2023_cristina.val@iticbcn.cat>
2023-12-01 13:44:18 +0100

    exercici mètodes acabat

---
+++ exercici6metodes.java
@@ -0,0 +1,455 @@
+public class exercici6metodes {
+//void significa que NO va a retornar nada
+//[CodingBat1]Contar si las palabras acaban en Z o Y 
+
+                                                            //public int countYZ(String str) {
+                                                            
+                                                                int countYZ = 0;
+                                                                int len = str.length();
+                                                                        
+                                                                for(int i = 0; i<len-1;i++) {
+                                                                    char carSeguent = str.charAt(i + 1);
+                                                                
+                                                                    if (!Character.isLetter(carSeguent)) {
+                                                                        char car = str.charAt(i);
+                                                                        if (car == 'y' || car == 'z' || car == 'Y' || car == 'Z') {
+                                                                            countYZ++;
+                                                                        }
+                                                                    }
+                                                                }
+                                                                        
+                                                                //ultima lletra es y o z?
+                                                                char car = str.charAt(len-1);
+                                                                if (car == 'y' || car == 'z' || car == 'Y' || car == 'Z') {
+                                                                    countYZ++;
+                                                                }
+                                                                
+                                                                return countYZ;
+
+//En el inteliJ
+public class Main {
+
+    public static int countYZ(String str) { //bloque ejercicio
+        //str = str.toLowerCase();
+        int countYZ = 0;
+        int len = str.length();
+        for(int i=0; i<len -1; i++){
+            char carSeguent = str.charAt(i+1);
+            if (!Character.isLetter(carSeguent)){
+                char car = str.charAt(i);
+                if (car =='Y' || car=='y' || car=='z' || car== 'Z'){
+                    countYZ++;
+                }
+            }
+        }
+        //si ultima lletra és y o z?
+        char car = str.charAt(len - 1);
+        if (car =='Y' || car=='y' || car=='z' || car== 'Z'){
+            countYZ++;
+        }
+
+        return countYZ;
+    }
+
+
+   public static void main(String[] args) { //bloque Main psvm
+        String str;
+        int resultat;
+        int resultatEsperat;
+
+        str = "fez day";
+        resultatEsperat = 2;
+        resultat = countYZ(str);
+        if (resultat == 2) {
+            System.out.println(str + " " + "OK");
+        } else {
+            System.out.println(str + " " + "X");
+        }
+
+        str = "day fez";
+        resultatEsperat = 2;
+        resultat = countYZ(str);
+        if (resultat == 2) {
+            System.out.println(str + " " + "OK");
+        } else {
+            System.out.println(str + " " + "X");
+        }
+
+        str = "day fyyyz";
+        resultatEsperat = 2;
+        resultat = countYZ(str);
+        if (resultat == 2) {
+            System.out.println(str + " " + "OK");
+        } else {
+            System.out.println(str + " " + "X");
+        }
+    }
+
+/*[CodingBat2] Dado 2 cadenas que una reste a la otra, ejemplo: "hello there", "llo" --> "he there"
+                                                                "hello there", "e" --> "hllo thr"       */
+
+                                                                    public String withoutString(String base, String remove) {
+                                                                    
+                                                                        String result = "";
+                                                                        for (int i = 0; i < base.length(); i++) {
+                                                                        
+                                                                            if (base.substring(i).toLowerCase().startsWith(remove.toLowerCase())) {     // Compara subcadenas sin distinguir mayúsculas y minúsculas
+                                                                                i = i + remove.length() - 1;                                            // Avanza el índice según la longitud de 'remove'
+                                                                            } else {                                                     
+                                                                                result = result +  base.charAt(i);                                      // Agrega el carácter actual a 'result'
+                                                                            }
+                                                                        }
+                                                                        return result;
+                                                                    }
+//En el inteliJ
+
+public class codingbat2 {
+
+    public static String palabra(String base, String remove) {
+        String result = "";
+
+        for (int i = 0; i < base.length(); i++) {
+            if (base.substring(i).toLowerCase().startsWith(remove.toLowerCase())) {
+                i = i + remove.length() - 1;
+            } else {
+                result = result +  base.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+        String stremove = "e";
+        String result = palabra(str, stremove);
+        
+        System.out.println (result);
+    }
+}
+
+
+
+//Calcular l'àrea i el perímetre d'una circumferència donat el radi
+
+public class AreaCercle {
+
+    //CAPÇALERA
+    //public static es posa sempre
+    //double és el tipus de dada que retorna la funcio o mètode
+    //areaCercle és el nom de la funcio
+    //els paràmetres (entre parèntesis) indiquen els valors que passem
+    
+    //COS és un programa com els que feiem al main
+    // es declaren variables (locals)
+    // retorna un valor
+
+    public static double areaCercle(double radi){
+        // radi està inicialitzat
+
+
+        return Math.PI * radi * radi;
+
+        
+        /* @param radi
+         * @return retorna l'area d'un cercle donat el radi.
+         */
+    }
+
+    public static double perimetreCercle(double radi){
+        return Math.PI * 2 * radi;
+
+        
+        /* @param radi
+         * @return retorna el perimetre d'un cercle donat el radi
+         */
+    }
+
+    //el main és el mètode que s'executa primer
+    public static void main(String[] args) {
+        
+        double crida1 = areaCercle(1);
+        System.out.println(crida1);
+        
+        //si el resultat és numèric, el podem fer servir en una expressió
+        System.out.println(3 * areaCercle(4));
+        
+        System.out.println(perimetreCercle(1));
+        if (perimetreCercle(1)!= 2 * Math.PI){
+            System.out.println("ERROR");
+        }
+    }
+}
+
+
+
+
+
+
+//Exercicis
+
+public class ExercicisVocals {
+
+    public static String vocals (){                     //1r mètode
+        return "aeiou";
+    }
+
+
+    public static String vocals (boolean majuscules){  //2n mètode
+
+        if (majuscules) {
+            return "AEIOU";
+        }else{
+            return "aeiou";
+        }
+    }
+
+
+    public static boolean aprovat (int nota){           //3 mètode
+
+        if (nota >=5){
+            return true;
+        }
+        return false;
+        }
+
+
+    public static int valorAbsolut (int numero){        //4t mètode
+
+        if (numero<0){
+            numero = - numero;
+        }
+        return numero;        //también se puede return Math.abs(numero);
+    }
+
+
+    public static int diferenciaValorAbsolut (int nu1, int nu2){   //5è mètode
+
+        int num1 = valorAbsolut(nu1);
+        int num2 = valorAbsolut(nu2);
+        return valorAbsolut(num1 - num2);
+    }
+
+
+    public static void main(String[] args){
+
+        System.out.println(vocals()); //1r mètode
+
+        System.out.println(vocals(true)); //2n mètode. Si true mayúsculas, si false minúsculas
+
+        System.out.println(aprovat(9)); // 3r mètode. Si aprova true, sino false
+
+        System.out.println(valorAbsolut(-16));  //4t mètode. Retornar el seu valor Absolut
+
+        System.out.println(diferenciaValorAbsolut(6,8)); //5è mètode. Retornar el valor Absolut de la diferencia de 2 num
+    }
+}
+
+
+
+//[CodingBat1: GHappy] Si hay xxggxx seguidas en un string = true, sino false
+                                                            //sin los tests
+                                                            import java.util.Scanner;
+                                                            public class codingBat1 {
+
+                                                                public static boolean gHappy (String str) {
+
+                                                                    boolean trobat = true;
+
+                                                                    for (int i = 0; i<str.length() && trobat; i++){
+                                                                        char c = str.charAt(i);
+
+                                                                        if ( c == 'g'){
+                                                                            if ((i>0 && str.charAt (i-1) == 'g') || (i<str.length() -1) && str.charAt(i+1) == 'g'){
+
+                                                                                trobat = true;
+                                                                            }else{
+                                                                                trobat = false;
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                    return trobat;
+                                                                }
+
+                                                                public static void main(String[] args) {
+                                                                    Scanner entrada = new Scanner(System.in);
+                                                                    System.out.println(gHappy(entrada.nextLine()));
+                                                                }
+                                                            }
+
+//[CodingBat: GHappy] InteliJ Con los tests 
+
+public class codingBat1 {
+
+    public static boolean gHappy (String str) {
+
+        boolean trobat = true;
+
+        for (int i = 0; i<str.length() && trobat; i++){
+            char c = str.charAt(i);
+
+            if ( c == 'g'){
+                if ((i>0 && str.charAt (i-1) == 'g') || (i<str.length() -1) && str.charAt(i+1) == 'g'){
+
+                    trobat = true;
+                }else{
+                    trobat = false;
+                }
+            }
+        }
+        return trobat;
+    }
+
+    public static void testgHappy (String str, boolean resultatEsperat){
+
+        boolean resultat = gHappy(str);
+        if ( resultat == resultatEsperat ) {
+            System.out.println(str + " " + "OK");
+        }else{
+            System.out.println(str + " " + "X");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        testgHappy("xxggxx", true);
+        testgHappy("xxgxx", false);
+        testgHappy("xxggyygxx", false);
+        testgHappy("g", false);
+        testgHappy("gg", true);
+        testgHappy(" ", true);
+        testgHappy("xxgggxyz", true);
+        testgHappy("xxgggxyg", false);
+        testgHappy("xxgggxygg", true);
+        testgHappy("mgm", false);
+        testgHappy("mggm", true);
+        testgHappy("yyygggxyy", true);
+    }
+}
+
+
+
+/*[CodingBat 2] CountTriple amb testos inclòs. Contar la cantidad de repeticiones que estan repetidas 3 veces seguidas.
+                                                ejemplo: xxxabyyyycd --> 3                                                  */
+public class codingBat2 {
+    public static int countTriple (String str) {
+
+        int contador = 0;
+
+        for (int i = 0; i<str.length()-2 ; i++){
+            char c = str.charAt(i);
+
+            if (c == str.charAt(i+1) && c == str.charAt(i+2)){
+                contador++;
+            }
+        }
+        return contador;
+    }
+    
+    public static void testCountTriple (String str, int resultatEsperat){
+
+        int resultat = countTriple(str);
+        if (resultat == resultatEsperat) {
+            System.out.println(str + " " + "OK");
+        }else{
+            System.out.println(str + " " + "X");
+        }
+    }
+    
+    public static void main(String[] args) {
+
+        testCountTriple("abcXXXabc", 1);
+        testCountTriple("xxxabyyyycd", 3);
+        testCountTriple("a", 0);
+        testCountTriple(" ", 0);
+        testCountTriple("XXXabc", 1);
+        testCountTriple("XXXXabc", 2);
+        testCountTriple("XXXXXabc", 3);
+        testCountTriple("222abyyycdXXX", 3);
+        testCountTriple("abYYYabXXXXXab", 4);
+        testCountTriple("abYYXabXXYXXab", 0);
+        testCountTriple("abYYXabXXYXXab", 0);
+        testCountTriple("122abhhh2", 1);
+    }
+}
+
+
+//[CodingBat3]Si la cadena tiene un número, sumar todos los números. ej: aa1bc2d3 --> 6
+
+public class codingBat3 {
+    public static int sumDigits(String str) {
+        
+        int suma = 0;
+        for ( int i = 0; i<str.length(); i++){
+            char c = str.charAt(i);
+
+            if (Character.isDigit(c)){
+                suma = suma + Integer.parseInt(String.valueOf(c) );
+            }
+        }
+        return suma;
+    }
+    
+    public static void testsumDigits (String str, int resultatEsperat){
+        
+        int resultat = sumDigits(str);
+        if (resultat == resultatEsperat) {
+            System.out.println(str + " " + "OK");
+        }else{
+            System.out.println(str + " " + "X");
+        }
+    }
+    
+    public static void main(String[] args) {
+
+        testsumDigits("aa1bc2d3", 6);
+        testsumDigits("aa11b33", 8);
+        testsumDigits("Chocolate", 0);
+        testsumDigits("5hoco1a1e", 7);
+        testsumDigits("123abc123", 12);
+        testsumDigits("", 0);
+        testsumDigits("Hello", 0);
+        testsumDigits("X1z9b2", 12);
+        testsumDigits("5432a", 14);
+    }
+}
+
+
+
+
+
+/*[Acepta el reto] Introduciendo X numero(indica cantidad de fechas)        2               Salida
+                    Mirar si la fecha introducida es Navidad o no           13 2              NO
+                                                                            25 12             SI            */
+
+import java.util.Scanner;
+public class AceptaelretoEsnavidad {
+
+    /**
+     * Donat un dia i un mes escriu "Si" si és Nadal,
+     * "NO" si no ho és
+     *
+     * @param dia
+     * @param mes
+     */
+    public static void esNavidad(int dia, int mes) {
+
+        if (dia == 25 && mes == 12) {
+            System.out.println("SI");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int nCasos = entrada.nextInt();             //se introduce el número de casos y se va a ejectura el bucle tantas
+        for (int i = 0; i < nCasos; i++) {          //veces como número introducido
+            int dia = entrada.nextInt();
+            int mes = entrada.nextInt();
+            esNavidad(dia, mes);                    //hace que los valores introducidos pasen por el método
+        }
+    }
+}
+
